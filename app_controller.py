8fc845b77fce703aa14ef407db22677ba94b364a
lekaha <lekaha@gmail.com>
2014-07-06 13:16:49 +0800

    Initial project
    The first time to commit for initialization.

---
+++ app_controller.py
@@ -0,0 +1,23 @@
+'''
+Name: app_controller
+Created on Jul 6, 2014
+Description:
+
+@author: lekaha
+
+History:
+Who          When                  What
+---------------------------------------
+'''
+
+class Controller(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
